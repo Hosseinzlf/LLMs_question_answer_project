67d80eec0bb3e1dd4d3093e3731904bc8f11e015
Hossein Zolfaghari <digimat2017@gmail.com>
2025-10-28 15:47:49 +0100

    started to develope main.py code from correct working main.ipynb file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage
-import secret_key
-api_key = secret_key.GOOGLE_API_KEY
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",api_key=api_key)
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-# Simple text invocation
-result = llm.invoke("Give me a joke.")
-print(result.content)
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+google_api_key = os.environ["GOOGLE_API_KEY"]
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",api_key=llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",api_key=api_key, temprature=0)
+)
